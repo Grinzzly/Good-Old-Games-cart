352735a39ca2fd5f9acce64f40966d1fee4433b7
Sergey Grin <grinzzly@gmail.com>
2019-11-08 17:45:35 +0100

    Secret button secretly implemented

--- src/modules/jumbotron/jumbotron.component.ts
+++ src/modules/jumbotron/jumbotron.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './jumbotron.component.html',
   styleUrls: ['./jumbotron.component.scss']
 })
-export class JumbotronComponent { }
+export class JumbotronComponent {
+  public bubbleIt(e) {
+    e.preventDefault();
+
+    e.target.classList.remove('animate');
+    e.target.classList.add('animate');
+
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  }
+}
