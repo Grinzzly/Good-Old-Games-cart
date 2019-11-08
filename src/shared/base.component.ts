532590ac57f311db59e1aa3430f67693182c5729
Sergey Grin <grinzzly@gmail.com>
2019-11-08 22:22:34 +0100

    Provide cart-view overlay

--- src/shared/base.component.ts
+++ src/shared/base.component.ts
@@ -1,45 +1,23 @@
 import {
   OnChanges,
   OnDestroy,
-  SimpleChanges,
-  AfterViewInit
+  SimpleChanges
 } from '@angular/core';
 
 import {
-  merge,
-  of,
   Subject,
-  Observable,
   Unsubscribable as AnonymousSubscription
 } from 'rxjs';
 
-import { map, filter, delay } from 'rxjs/operators';
-
 export class BaseComponentTyped<TActual extends BaseComponentTyped<TActual>>
-  implements OnChanges, OnDestroy, AfterViewInit {
-
-  protected get viewInitialized$(): Observable<void> {
-    if (!this._viewInitialized$) {
-      this._viewInitialized$ = new Subject<void>();
-    }
-    return this._viewInitialized$.pipe(delay(0));
-  }
+  implements OnChanges, OnDestroy {
 
   private subscriptions: { [key: string]: AnonymousSubscription } = {};
 
-  private _changes$ = new Subject<SimpleChanges>();
-
-  private _viewInitialized$: Subject<void>;
-
-  public ngAfterViewInit() {
-    if (this._viewInitialized$) {
-      this._viewInitialized$.next();
-      this._viewInitialized$.complete();
-    }
-  }
+  private changes$ = new Subject<SimpleChanges>();
 
   public ngOnChanges(changes: SimpleChanges) {
-    this._changes$.next(changes);
+    this.changes$.next(changes);
   }
 
   public ngOnDestroy() {
@@ -51,27 +29,14 @@ export class BaseComponentTyped<TActual extends BaseComponentTyped<TActual>>
   }
 
   public createGuid(): string {
-    let guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const guid = 'xxxxxxxx-yxxx-yxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
       // tslint:disable-next-line:no-bitwise
-      let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      const radix = Math.random() * 16 | 0, value = c === 'x' ? radix : (radix & 0x3 | 0x8);
 
-      return v.toString(16);
+      return value.toString(16);
     });
 
     return guid;
-  }
-
-  protected observeInputProperty<TKey extends keyof TActual>(
-    propertyName: TKey,
-    includeCurrent?: boolean
-  ): Observable<TActual[TKey]> {
-    const changes$ = this._changes$.pipe(
-      filter(changes => changes.hasOwnProperty(propertyName)),
-      map(changes => changes[String(propertyName)].currentValue));
-
-    return includeCurrent
-      ? merge(of(this[`${propertyName}`]), changes$)
-      : changes$;
   }
 
   protected addSubscription(subscription: AnonymousSubscription, key: string = undefined) {
@@ -80,12 +45,14 @@ export class BaseComponentTyped<TActual extends BaseComponentTyped<TActual>>
     } else {
       this.removeSubscription(key);
     }
+
     this.subscriptions[key] = subscription;
   }
 
   protected removeSubscription(key: string) {
     if (this.subscriptions[key]) {
       this.subscriptions[key].unsubscribe();
+
       delete this.subscriptions[key];
     }
   }
